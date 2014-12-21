668002c010a2fe734d5b428452aee5b4d45d6387
Ryan L. Bell <ryan.l.bell@gmail.com>
2014-12-21 18:58:51 -0500

    Remove Math.sign usage to remain compatible w/ Safari.

--- flip-png/index.js
+++ flip-png/index.js
@@ -21,7 +21,7 @@ var Game = (function(PIXI) {
       this.sprite.scale.x += this.sprite.scaleFactor;
       if (Math.abs(this.sprite.scale.x) >= 1) {
         this.sprite.scaleFactor = 0;
-        this.sprite.scale.x = Math.sign(this.sprite.scale.x) * 1;
+        this.sprite.scale.x = sign(this.sprite.scale.x) * 1;
       }
     }
 
@@ -48,7 +48,7 @@ var Game = (function(PIXI) {
 
   Game.prototype.flipSprite = function() {
     if (!this.scaleFactor) {
-      this.scaleFactor = Math.sign(this.scale.x) * -0.05;
+      this.scaleFactor = sign(this.scale.x) * -0.05;
     }
   }
 
@@ -58,6 +58,14 @@ var Game = (function(PIXI) {
 
     sprite.position.x = 300;
     sprite.position.y = 200;
+  }
+
+  function sign(num) {
+    var val = Number(num);
+    if (val === 0 || val !== val) {
+      return val;
+    }
+    return val > 0 ? 1 : -1;
   }
 
   return Game;
